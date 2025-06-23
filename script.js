73c2bca1abfa93414ddd98837c20b9dec82dee62
Pragyan Bimali <pragyanb02@gmail.com>
2025-06-23 21:55:28 +1000

    Add essential comments

--- script.js
+++ script.js
@@ -107,11 +107,13 @@ const DisplayController = (function(){
 
 // Calls all the above methods and simulates a game
 const GameController = (function(){
+    // Declare some query selectors
     const boardCells = document.querySelectorAll(".cell");
     const gameContainer = document.querySelector(".gameboard-container");
     const turnText = document.querySelector(".turn-text");
     const playerDetailForm = document.querySelector(".playerDetailForm");
 
+    // Declare some variables withing the GameController function scope so that the sub-functions can access them
     let playerX;
     let playerO;
     let currentPlayer;
@@ -122,27 +124,33 @@ const GameController = (function(){
         
         // Initially display the Modal
         DisplayController.openDialogAskName();
+        // Then render in the frontend
         DisplayController.render();
+        // We make the Game Container invisible while the Player(s) is/are asked to enter their names
         gameContainer.style.opacity = 0;
+        // Same with the turn texts.
         turnText.style.opacity = 0;
     }
 
-    // Now we ge the player details - for Player X and Player O
+    // Now once the Start Game button of the initial form is clicked.....
     playerDetailForm.addEventListener("submit", function(e) {
+        // Prevents the default behaviour
         e.preventDefault();
-
+        // Now we read the form input values to give the names to the players
         let playerXName = e.target.elements.playerX.value;
         let playerOName = e.target.elements.playerO.value;
-
+        // Then we instanciate the PlayerModule to create player objects with the above names and markers based on whether the Player is Player X or O
         playerX = PlayerModule.createPlayer(playerXName, "X");
         playerO = PlayerModule.createPlayer(playerOName, "O");
+        // We set the current Player to Player X so that X starts first
         currentPlayer = playerX;
-
+        // Now we set the turntext value to say that it's the currentPlayer's turn which in this case is Player X since X starts first
         turnText.textContent = `It's ${currentPlayer.name}'s turn!`
-
+        // We close the modal once the submit button is clicked
         DisplayController.closeDialogAskName();
-
+        // Then we display the Game Container which is the Gameboard grid
         gameContainer.style.opacity = 1;
+        // And we also display the turntext to display whose turn to play it is
         turnText.style.opacity = 1;
     });
 
@@ -184,7 +192,7 @@ const GameController = (function(){
             DisplayController.alertMsg(`${currentPlayer.name} is the winner!`)    
         }
 
-        // Check for draw
+        // Check for draw. If the board is full and no other win condition from the above is met
         const isBoardFull = Gameboard.gameboard.flat().every(cell => cell !== "");
         if (isBoardFull) {
             DisplayController.alertMsg("It's a tie!") 
@@ -196,21 +204,22 @@ const GameController = (function(){
     // Add click events for each cell in the Gameboard UI
     boardCells.forEach(cell => {
     cell.addEventListener("click", function() {
-        
+        // Now we get the row and column values using the data-row/column value of each cell
         let row = parseInt(cell.dataset.row);
         let column = parseInt(cell.dataset.column);
-
+        // Now if the cell the player is clicking on is not empty, we display the below message and then return to the click event stack again.
         if (Gameboard.gameboard[row][column] !== "") {
             DisplayController.alertMsg("Cell already Marked!");
             return;
         }
-
+        // If empty, we set the gameboard array item value to the marker of the currentPlayer
         Gameboard.gameboard[row][column] = currentPlayer.marker;
-        DisplayController.render();
-
+        DisplayController.render(); // Then render it on the frontend.
+        // We then actively check for any winner
         checkForWinner();
+        // Then we switch the player. If current player is X then we switch to O and vice-versa
         currentPlayer = currentPlayer === playerX ? playerO : playerX;
-        
+        // Then we change the turntext to another player
         turnText.textContent = `It's ${currentPlayer.name}'s turn!`
     })
 })
